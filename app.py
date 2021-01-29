cb91a54ed730f3e23403cc64f133166998693b2c
Om1510-coder <78219068+Om1510-coder@users.noreply.github.com>
2021-01-29 20:24:48 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler('DC2CafVgKapteW2M0oevT5GAq', 'ySKfUJ3cYTEiFDBTsV8gAOHLktcheSmvsj528VUSbTfqywrGvG')
+auth.set_access_token('1317765409508970496-FPBBYybTrVwNQ95z27YkyUUie4x7a8', '2oV2p5U0AS7KezhgnautzJr2RmqYHnAVf6jzCXytNFEmw')
+
+api = tweepy.API(auth)
+while True:
+  user = api.get_user('OmSuneel')
+  f = user.followers_count
+  api.update_profile(name=f'OmSuneel {f} Followers')
+  print(f'OmSuneel {f} Followers')
+  time.sleep(60)
